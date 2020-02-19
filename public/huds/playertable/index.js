9ea02907893a672fe4bae84dc502ba5237f36442
Susanna Lepola <lepolsussu@gmail.com>
2020-02-19 21:43:39 +0200

    Add new HUD and delete old HUDS

--- public/huds/playertable/index.js
+++ public/huds/playertable/index.js
@@ -4,8 +4,6 @@ var team_two = [];
 var teams = [{"sjgaming": ["76561198095859748", "76561198166952201", "76561198026620771", "76561197986831501", "76561197963335479"],"havugaming": ["76561198148047557", "76561198038632729", "76561198002132867", "76561197972265203", "76561197976197744"]}];
 
 function updatePage(data) {
-    
-    
     var players = data.getPlayers();
     var teamonedata = data.getTeamOne();
     var teamtwodata = data.getTeamTwo();
@@ -19,123 +17,110 @@ function updatePage(data) {
     team_one = teams[0][teamoneid];
     team_two = teams[0][teamtwoid];
 
-    
-    
-//    console.log(players);
+    $('#team1').find(".team_logo").css({
+        backgroundImage: "url(/teams/"+teamonedata.logo + ")"
+    });
+
+    $('#team2').find(".team_logo").css({
+        backgroundImage: "url(/teams/"+teamtwodata.logo + ")"
+    });
     
     var i = 0;
-    var steamidlist = "";
-    var st = 0;
-    
+
     while(i < players.length) {
         
         if(team_one[0] == players[i].steamid) {
             fillPlayer(0, players[i]);
-            rotateHealth(players[i].state.health,1);
         }
         
         if(team_one[1] == players[i].steamid) {
             fillPlayer(1, players[i]);
-            rotateHealth(players[i].state.health,2);
         }
         
         if(team_one[2] == players[i].steamid) {
             fillPlayer(2, players[i]);
-            rotateHealth(players[i].state.health,3);
         }
         
         if(team_one[3] == players[i].steamid) {
             fillPlayer(3, players[i]);
-            rotateHealth(players[i].state.health,4);
         }
         
         if(team_one[4] == players[i].steamid) {
             fillPlayer(4, players[i]);
-            rotateHealth(players[i].state.health,5);
         }
         
         if(team_two[0] == players[i].steamid) {
             fillPlayer(5, players[i]);
-            rotateHealth(players[i].state.health,6);
         }
         
         if(team_two[1] == players[i].steamid) {
             fillPlayer(6, players[i]);
-            rotateHealth(players[i].state.health,7);
         }
         
         if(team_two[2] == players[i].steamid) {
             fillPlayer(7, players[i]);
-            rotateHealth(players[i].state.health,8);
         }
         
         if(team_two[3] == players[i].steamid) {
             fillPlayer(8, players[i]);
-            rotateHealth(players[i].state.health,9);
         }
         
         if(team_two[4] == players[i].steamid) {
             fillPlayer(9, players[i]);
-            rotateHealth(players[i].state.health,10);
         }
-        
-//        if(st == 0) {
-//            steamidlist = steamidlist + ', "' + players[i].steamid + '"';
-//        }
-//        
-//        if(i == (players.length - 1)) {
-//            st = 1;
-//            console.log(steamidlist);
-//        }
         i++;
     }
 }
 
 function fillPlayer(slot, player) {
+    let statistics = player.getStats();
+    let team = player.team.toLowerCase();
+    let health_color = statistics.health <= 20 ? "#ff0000" : team == "ct" ? "#00a0ff":"#ffa000";
+    let gradient = "linear-gradient(to left, rgba(0,0,0,0) " + (100-statistics.health) + "%, " + health_color + " " + (100-statistics.health) + "%)";
+    
+    $('#player' + (slot + 1) + ' .hp_bar').css("background", gradient);
     $('#player' + (slot + 1) + ' .name').text(player.name);
-    $('#player' + (slot + 1) + ' #team_logo').html(
-        ('<img src="/teams/' + player.teamData.logo + '" />')
-);
+
+    var weapons = player.getWeapons();
+    var isprimary = false;
+    var weapleng = Object.keys(weapons).length;
+    var primarywep;
+    var secondwep;
+
+    for(let key in weapons){
+        let weapon = weapons[key];
+        let name = weapon.name.replace("weapon_", "");
+        let state = weapon.state;
+        let view = "";
+        let type = weapon.type;
+
+        if(type == "Rifle" || type == "SniperRifle") {
+            isprimary = true;
+            primarywep = name;
+        }
+
+        if(type == "Pistol") {
+            secondwep = name;
+        }
+
+        if(key == ("weapon_" + (weapleng - 1))) {
+            if(isprimary == true) {
+                $('#player' + (slot + 1) + ' .weapons').append($("<img />").attr("src", "/files/img/weapons/" + primarywep + ".png"));
+            } else {
+                $('#player' + (slot + 1) + ' .weapons').append($("<img />").attr("src", "/files/img/weapons/" + secondwep + ".png"));
+            }
+        }
+    }
+    
+    $('#player' + (slot + 1) + ' .numberstats #kills').text("K: " + statistics.kills);
+    $('#player' + (slot + 1) + ' .numberstats #assists').text("A: " + statistics.assists);
+    $('#player' + (slot + 1) + ' .numberstats #deaths').text("D: " + statistics.deaths);
+
     $('#player' + (slot + 1) + ' .stats .wrap .money').text("$" + player.state.money);
     $('#player' + (slot + 1) + ' .stats .wrap .hp').text("+" + player.state.health);
     if(player.state.health <= 0) {
         $('#player' + (slot + 1)).addClass("dead");
     } else {
         $('#player' + (slot + 1)).removeClass("dead");
-    }
-}
-
-function rotateHealth(hp, player) {
-    var deg = 3.6 * hp;
-    
-    if(hp > 50) {
-        $('#player' + player + ' .circle .circle-right-wrap').css({
-            display: "block"
-        });
-        
-        $('#player' + player + ' .circle').css({
-            width: "250px",
-            overflow: "visible"
-        });
-    } else {
-        $('#player' + player + ' .circle .circle-right-wrap').css({
-            display: "none"
-        });
-
-        
-        $('#player' + player + ' .circle').css({
-            width: "125px",
-            overflow: "hidden"
-        });
-    }
-    
-    if(hp <= 20) {
-        $('#player' + player + ' .circle .circle-left-wrap .circle-left').css({
-            borderColor: "red"
-        });
-    } else {
-        $('#player' + player + ' .circle .circle-left-wrap .circle-left').css({
-            borderColor: "#3e8ddd"
-        });
     }
 }
